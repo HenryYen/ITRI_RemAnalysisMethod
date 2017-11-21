80ef7b00d947c64a6fa553d2487932315b32d946
HenryYen <r821231199307@gmail.com>
2017-11-21 15:13:19 +0800

    add a function to draw bitmap which specifies location distribution of user reports

---
+++ Utility.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator
+
+
+map_size = [104, 26]
+
+def draw_heatmap(model):     # specialized drawing function for ITRI building 51
+    x_resolution = map_size[0]
+    y_resolution = map_size[1]
+    xaxis = np.linspace(0., x_resolution, x_resolution)
+    yaxis = np.linspace(0., y_resolution, y_resolution)  
+    x, y = np.meshgrid(xaxis, yaxis)
+    pos = np.column_stack((np.reshape(x, x_resolution*y_resolution), np.reshape(y, x_resolution*y_resolution)))  # [[x1, y1], [x2, y2]...]
+    z = model.predict(pos)
+    z = np.reshape(z, (y_resolution, x_resolution))    
+    plt.contourf(x, y, z, 500, cmap='jet')                             
+    plt.colorbar() 
+    plt.savefig('heapmap', dpi=500)
+    plt.show()
+
+
+def draw_bitmap(X_train):          # draw the location distribution of user reports
+    fig, ax = plt.subplots()
+    plt.axis([0, map_size[0], 0, map_size[1]])
+    minorLocator = MultipleLocator(1)   #spacing of grid : 1 unit
+    ax.yaxis.set_minor_locator(minorLocator)
+    ax.xaxis.set_minor_locator(minorLocator)
+
+    for i in range(len(X_train)):        
+        plt.plot(round(X_train[i][0])+0.5, round(X_train[i][1])+0.5, color='b', marker='d')  #shift x, y by 0.5 unit
+    plt.grid(which = 'minor', color='black', linestyle='-', linewidth=0.5)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title('Location of user report')
+    plt.savefig('bitmap.png', dpi=500)
+    plt.show()
+
--- train.py
+++ train.py
@@ -11,22 +11,17 @@ from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
 import statsmodels.api as sm
+from Utility import draw_heatmap, draw_bitmap
 
     
 path = './data'
-fn_train_data = path + '/MDT.csv'
-#fn_test_data = path + '/test.in'
-#fn_output = 'out.csv'
+fn_train_data = path + '/MDT_fix_user.csv'
 fn_model = 'model1'
 
-train_data_size =500
+train_data_size = 126
 test_data_size = 3000
-batch_size = 16
-nb_epoch = 150
+nb_epoch = 250
 nb_feature = 2      # feature : x, y
 
 
@@ -38,6 +33,7 @@ def load_train_data():
     y_test = dataset[train_data_size:train_data_size+test_data_size, -2]  
     return (X_train, y_train, X_test, y_test)
     
+
 
 def build_NN_model(X_train, y_train):     
     print "+++Deep NN"
@@ -52,9 +48,8 @@ def build_NN_model(X_train, y_train):
     model.add(Activation('relu'))
     model.add(Dense(1))
     model.compile(loss='mean_squared_error', optimizer='adam')
-    model.fit(X_train, y_train, nb_epoch=nb_epoch, batch_size=batch_size,  shuffle=True)
+    model.fit(X_train, y_train, nb_epoch=nb_epoch, batch_size=16,  shuffle=True)
     return model
-
 def build_non_linear_model(X_train, y_train):   #shallow NN, "non-deep" NN    
     print "+++Shallow NN"
     model = Sequential()    # first input layer
@@ -62,73 +57,49 @@ def build_non_linear_model(X_train, y_train):   #shallow NN, "non-deep" NN
     model.add(Dense(128, activation='relu'))    # 3rd hidden layer
     model.add(Dense(1))     # output layer
     model.compile(loss='mean_squared_error', optimizer='adam')    
-    model.fit(X_train, y_train, nb_epoch=nb_epoch, batch_size=batch_size,  shuffle=True)
+    model.fit(X_train, y_train, nb_epoch=nb_epoch, batch_size=16,  shuffle=True)
     return model
+
 
 def build_DTReg_model(X_train, y_train):
     print "+++Decision Tree"
     model = DecisionTreeRegressor(max_depth=30)
     model.fit(X_train, y_train)   
     return model
-
 def build_RFReg_model(X_train, y_train):
     print "+++Random Forest"
-    model = RandomForestRegressor(max_depth=30, random_state=2)
+    model = RandomForestRegressor(n_estimators=100, max_depth=30, random_state=2)
     model.fit(X_train, y_train)   
     return model
 
+
 def build_LinReg_model_sk(X_train, y_train, X_test): 
+    print "+++Linear regression"
     X_train = add_constant(X_train)   
     model = linear_model.LinearRegression()
     model.fit(X_train, y_train)   
     return model
-
 def build_LinReg_model_sm(X_train, y_train, X_test): 
     X_train = add_constant(X_train)   
     model = sm.OLS(y_train, X_train).fit()   
     print model.summary()
     return model
-
 def build_LinReg_model_numpy(X_train, y_train, X_test): 
     X_train = add_constant(X_train)
     model = np.linalg.lstsq(X_train,y_train)[0]
-    return model
-
-def build_gaussian_model(X_train, y_train, X_test):    
-    kernel = C(1.0, (1e-3, 1e3)) * RBF(10, (1e-2, 1e2))
-    model = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=9)
-    model.fit(X_train, y_train)   
-    return model
-    
+    return model    
 def add_constant(X):
     constant = np.array([[1]] * len(X))
     X = np.append(X, constant, axis=1)
     return X
-
-def get_heapmap(model):     # specialized drawing function for ITRI building 51
-    x_resolution = 104
-    y_resolution = 26
-    xaxis = np.linspace(0.,104.,x_resolution)
-    yaxis = np.linspace(0.,26.,y_resolution)  
-    x, y = np.meshgrid(xaxis, yaxis)
-    pos = np.column_stack((np.reshape(x, x_resolution*y_resolution), np.reshape(y, x_resolution*y_resolution)))  # [[x1, y1], [x2, y2]...]
-    z = model.predict(pos)
-    z = np.reshape(z, (y_resolution, x_resolution))
-    
-    plt.contourf(x, y, z, 1000, cmap='jet')                             
-    plt.colorbar() 
-    plt.show()
-    plt.savefig('heapmap')
     
     
-
     
 if __name__ == '__main__':
     try:
         print '***load data...'
         (X_train, y_train, X_test, y_test) = load_train_data()        
         print '***begin to train...'
-
         
         #model = build_NN_model(X_train, y_train)  
         #model = build_non_linear_model(X_train, y_train)       
@@ -146,8 +117,8 @@ if __name__ == '__main__':
         print("[MSE]: %.3f" % mean_squared_error(y_test, y_pred))
         print('[R2]: %.3f' % r2_score(y_test, y_pred))  
         print('[ExplainVariance]: %.3f' % explained_variance_score(y_test, y_pred)) 
-        #print('[MAE]: %.3f' % mean_absolute_error(y_test, y_pred))   
-        #get_heapmap(model)
+        draw_heatmap(model)
+        draw_bitmap(X_train)
         
     
     except KeyboardInterrupt:           
